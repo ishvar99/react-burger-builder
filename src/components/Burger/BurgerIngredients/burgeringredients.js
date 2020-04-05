6b28262017b002f05bf4a4e36d09c981e610c211
ishvar99 <ishanvarshney99@gmail.com>
2020-04-06 01:15:58 +0530

    added prop-types validation to BugerIngredients component

--- src/components/Burger/BurgerIngredients/burgeringredients.js
+++ src/components/Burger/BurgerIngredients/burgeringredients.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'; 
 import classes from './burgeringredients.css';
-const BurgerIngredients=(props)=>{
-
+class BurgerIngredients extends Component{
+render(){
     let ingredients=null;
-
-    switch(props.type){
+    switch(this.props.type){
         case('bread-bottom'):
             ingredients=<div className={classes.BreadBottom}></div>
             break;
@@ -29,4 +29,11 @@ const BurgerIngredients=(props)=>{
         default:
             ingredients=null;
     }
-}
+    return ingredients;
+ }
+ }
+ BurgerIngredients.propTypes={
+    type:PropTypes.string.isRequired
+ }
+
+ export default BurgerIngredients
